31222e87589a18e68e8c08fb9be02d07d03ea6e5
novakvova <novakvova@gmail.com>
2024-07-14 10:18:28 +0300

    Working Simple MVC

---
+++ WebPizzaSite/WebPizzaSite/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPizzaSite.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ WebPizzaSite/WebPizzaSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebPizzaSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
