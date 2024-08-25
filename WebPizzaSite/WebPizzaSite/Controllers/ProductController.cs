a24683ed3d230e764d1bf560d21a0de80dfc07a5
novakvova <novakvova@gmail.com>
2024-08-25 11:34:15 +0300

    Add Product is good

--- WebPizzaSite/WebPizzaSite/Controllers/ProductController.cs
+++ WebPizzaSite/WebPizzaSite/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebPizzaSite.Data;
+using WebPizzaSite.Data.Entities;
 using WebPizzaSite.Models.Product;
 
 namespace WebPizzaSite.Controllers
@@ -9,11 +12,14 @@ namespace WebPizzaSite.Controllers
     public class ProductController : Controller
     {
         private readonly PizzaDbContext _pizzaDbContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly IMapper _mapper;
-        public ProductController(PizzaDbContext pizzaDbContext, IMapper mapper)
+        public ProductController(PizzaDbContext pizzaDbContext, IMapper mapper,
+            IWebHostEnvironment webHostEnvironment)
         {
             _pizzaDbContext = pizzaDbContext;
             _mapper = mapper;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -21,6 +27,60 @@ namespace WebPizzaSite.Controllers
                 .ProjectTo<ProductItemViewModel>(_mapper.ConfigurationProvider)
                 .ToList();
             return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var catList = _pizzaDbContext.Categories
+                .Select(x=>new {Value = x.Id, Text = x.Name})
+                .ToList();
+
+            ProductCreateViewModel model = new ProductCreateViewModel();
+            model.CategoryList = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(catList, "Value", "Text");
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var prod = new ProductEntity
+            {
+                Name = model.Name,
+                Price = model.Price,
+                CategoryId = model.CategoryId,
+            };
+
+            await _pizzaDbContext.Products.AddAsync(prod);
+            await _pizzaDbContext.SaveChangesAsync();
+            if (model.Photos != null)
+            {
+                int i = 0;
+
+                foreach (var img in model.Photos)
+                {
+                    string ext = System.IO.Path.GetExtension(img.FileName);
+                    string fileName = Guid.NewGuid().ToString() + ext;
+                    var path = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", fileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await img.CopyToAsync(stream);
+                    }
+                    var imgEntity = new ProductImageEntity
+                    {
+                        Name = fileName,
+                        Priority = i++,
+                        Product = prod,
+                    };
+                    _pizzaDbContext.ProductImages.Add(imgEntity);
+                    _pizzaDbContext.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ WebPizzaSite/WebPizzaSite/Models/Product/ProductCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebPizzaSite.Models.Product
+{
+    public class ProductCreateViewModel
+    {
+        [Display(Name="Назва")]
+        [Required(ErrorMessage ="Вкажіть назву продукту")]
+        [StringLength(250, ErrorMessage ="Назва продукту не може перевищувати 250 символів")]
+        public string Name { get; set; } = null!;
+        [Display(Name="Ціна")]
+        public decimal Price { get; set; }
+        [Display(Name="Категорія")]
+        public int CategoryId { get; set; }
+        [Display(Name="Оберіть список фото продукта")]
+        public List<IFormFile>? Photos { get; set; }
+        //Випадаючий список із категоріями
+        public SelectList? CategoryList { get; set; }
+
+    }
+}
