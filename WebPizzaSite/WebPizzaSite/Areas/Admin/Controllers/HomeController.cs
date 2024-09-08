6523ad5199c2f448375e399ec24832a7aaf31489
novakvova <novakvova@gmail.com>
2024-09-08 11:52:39 +0300

    Admin Panel working navigation

--- WebPizzaSite/WebPizzaSite/Areas/Admin/Controllers/HomeController.cs
+++ WebPizzaSite/WebPizzaSite/Areas/Admin/Controllers/HomeController.cs
@@ -17,5 +17,58 @@ namespace WebPizzaSite.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult Accordion()
+        {
+            return View();
+        }
+        public IActionResult Badges()
+        {
+            return View();
+        }
+        public IActionResult Breadcrumbs()
+        {
+            return View();
+        }
+        public IActionResult Buttons()
+        {
+            return View();
+        }
+        public IActionResult Cards()
+        {
+            return View();
+        }
+        public IActionResult Carousel()
+        {
+            return View();
+        }
+        public IActionResult ListGroup()
+        {
+            return View();
+        }
+        public IActionResult Modal()
+        {
+            return View();
+        }
+        public IActionResult Tabs()
+        {
+            return View();
+        }
+        public IActionResult Pagination()
+        {
+            return View();
+        }
+        public IActionResult Progress()
+        {
+            return View();
+        }
+        public IActionResult Spinners()
+        {
+            return View();
+        }
+        public IActionResult Tooltips()
+        {
+            return View();
+        }
     }
 }
