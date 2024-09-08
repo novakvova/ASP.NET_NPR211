bf5603825a085bace5974bfe2358b44931f59cb5
novakvova <novakvova@gmail.com>
2024-09-08 11:02:29 +0300

    Admin Stucture

--- WebPizzaSite/WebPizzaSite/Areas/Admin/Controllers/HomeController.cs
+++ WebPizzaSite/WebPizzaSite/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebPizzaSite.Constants;
 
 namespace WebPizzaSite.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = Roles.Admin)]
     public class HomeController : Controller
     {
         public IActionResult Index()
