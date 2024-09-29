f85c38315a0eaf90076c195d4371670963618bf5
novakvova <novakvova@gmail.com>
2024-09-29 12:55:15 +0300

    Add Simple form create category

--- my-app/src/components/category/create/index.js
+++ my-app/src/components/category/create/index.js
@@ -1,8 +1,35 @@
+import {Link} from "react-router-dom";
+
 const CategoryCreatePage = () => {
     return (
         <>
             <div className={"container"}>
                 <h1 className={"text-center"}>Додати категорію</h1>
+                <form className={"col-md-6 offset-md-3"}>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Назва</label>
+                        <input type="text" className="form-control" id="name" name={"name"}/>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="imageFile" className="form-label">Фото</label>
+                        <input className="form-control" type="file" id="imageFile" name={"imageFile"}/>
+                    </div>
+
+                    <div className="form-floating mb-3">
+                        <textarea className="form-control" placeholder="Вкажіть опис"
+                                  name={"description"}
+                                  id="description"
+                                  style={{height: "100px"}}></textarea>
+                        <label htmlFor="description">Опис</label>
+                    </div>
+
+                    <div className="mb-3 d-flex justify-content-center">
+                        <Link to={"/"} className="btn btn-info mx-2">Скасувати</Link>
+                        <button type="submit" className="btn btn-primary">Додати</button>
+                    </div>
+
+                </form>
             </div>
         </>
     )
