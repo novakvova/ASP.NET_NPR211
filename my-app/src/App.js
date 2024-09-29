a39c3cc4a524672137bfa5d1f0567a3e177438a5
novakvova <novakvova@gmail.com>
2024-09-29 09:41:03 +0300

    Simple State and Map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [list, setList] = useState([
+        {
+            id: 1,
+            name: "Ковбаса",
+            image: "https://sardelka.com.ua/wp-content/uploads/2023/04/Pryprava-kovbasa-Matsykova-ta-matsik-Poliskyj.jpg"
+        }
+    ]);
+    console.log("List items", list);
+
+    const handleAddButton = () => {
+        setList([
+            ...list, {
+            id: 2,
+            name: "Масло",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXPA1yX3G4SdQRpEDjr56wVaYCPqJwsxsdVg&s"
+        }]);
+    }
+
+    return (
+        <>
+            <div className="container">
+                <h1 className="text-center">Список категорій</h1>
+                <button className={"btn btn-success"} onClick={handleAddButton}>Додати новий</button>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Фото</th>
+                        <th scope="col">Назва</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {list.map((item) => (
+                        <tr>
+                            <th scope="row">{item.id}</th>
+                            <td>
+                                <img src={item.image} alt={item.name} width="75px" />
+                            </td>
+                            <td>{item.name}</td>
+                        </tr>
+                    ))}
+
+                    </tbody>
+                </table>
+            </div>
+
+
+        </>
+    );
 }
 
 export default App;
