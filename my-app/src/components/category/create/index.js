a9dcf3dd2daa8623bbad5949f3ecd84d1f503455
novakvova <novakvova@gmail.com>
2024-09-29 13:18:18 +0300

    Create category is good

--- my-app/src/components/category/create/index.js
+++ my-app/src/components/category/create/index.js
@@ -1,25 +1,61 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
 
 const CategoryCreatePage = () => {
+    const navigate = useNavigate();
+    const [data, setData] = useState({
+        name: "", //назва категорії
+        description: "", //опис категорії
+        imageFile: null //файлу немає
+    });
+
+    const handlerOnChange = (e) => {
+        setData({...data, [e.target.name]: e.target.value});
+    }
+
+    const handlerOnFileChange = (e) => {
+        //console.log("Files", e.target.files[0]);
+        setData({...data, imageFile: e.target.files[0]});
+    }
+    //console.log(data);
+    const handlerOnSubmit = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:5290/api/categories", data,
+            {
+                headers: {"Content-Type": "multipart/form-data"},
+            })
+            .then(res => {
+                navigate("/");
+            });
+        //console.log("State send", data);
+    }
     return (
         <>
             <div className={"container"}>
                 <h1 className={"text-center"}>Додати категорію</h1>
-                <form className={"col-md-6 offset-md-3"}>
+                <form onSubmit={handlerOnSubmit} className={"col-md-6 offset-md-3"}>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Назва</label>
-                        <input type="text" className="form-control" id="name" name={"name"}/>
+                        <input type="text" className="form-control"
+                               id="name" name={"name"}
+                               value={data.name}
+                               onChange={handlerOnChange}
+                        />
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="imageFile" className="form-label">Фото</label>
-                        <input className="form-control" type="file" id="imageFile" name={"imageFile"}/>
+                        <input className="form-control" type="file" id="imageFile" name={"imageFile"}
+                            onChange={handlerOnFileChange}/>
                     </div>
 
                     <div className="form-floating mb-3">
                         <textarea className="form-control" placeholder="Вкажіть опис"
                                   name={"description"}
                                   id="description"
+                                  value={data.description}
+                                  onChange={handlerOnChange}
                                   style={{height: "100px"}}></textarea>
                         <label htmlFor="description">Опис</label>
                     </div>
